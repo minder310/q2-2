1aab83185d7d6a20a8c97a1f98e38a25de498805
Pierre.K <minder309@gmail.com>
2023-02-24 08:42:34 +0800

    pop網頁設計到一半。

--- back/pop.php
+++ back/pop.php
@@ -1,13 +1,25 @@
 最新文章管理
-<table>
-    <tr>
-        <td>編號</td>
-        <td>標題</td>
-        <td>顯示</td>
-        <td>刪除</td>
-    </tr>
-    <?php
-    $news=$News->all();
-    // dd($news); 
-    ?>
-</table>
+
+<form action="">
+    <table>
+
+        <tr>
+            <td>編號</td>
+            <td>標題</td>
+            <td>顯示</td>
+            <td>刪除</td>
+        </tr>
+        <?php
+        $news = $News->all();
+        // dd($news); 
+        foreach ($news as $key) { ?>
+            <tr>
+                <td><?= $key['id'] ?></td>
+                <td><?= $key['title'] ?></td>
+                <td><input type="checkbox" name="sh[]" id="sh" value="<?= $key['id'] ?>"></td>
+                <td><input type="checkbox" name="del[]" id="sh" value="<?= $key['id'] ?>"></td>
+            </tr>
+        <?php } ?>
+    </table>
+    <button>確定修改</button>
+</form>
